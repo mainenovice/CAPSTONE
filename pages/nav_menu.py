19826898f0532eeab07f57d7da6d4982e1259079
if_name_is_maine <andy.j.theriault@gmail.com>
2024-02-21 22:25:49 -0500

    Delete pages/nav_menu.py

--- pages/nav_menu.py
+++
@@ -1,7 +0,0 @@
-import flet as ft
-
-class Nav_Menu():
-    menu_add = ft.ElevatedButton(text='Add/Update Cultures', on_click=lambda _:page.go('/add'))
-    menu_about = ft.ElevatedButton(text='Cultures Data', on_click=lambda _:page.go('/cultures'))
-    menu_notes = ft.ElevatedButton(text='Take New Notes', on_click=lambda _:page.go('/notes'))
-    menu_row = ft.Row(controls=[menu_add, menu_about, menu_notes], alignment=ft.MainAxisAlignment.CENTER)
